706bfa8be1a74e7dca7566dd10f1997fcdf0ef3d
alanbacay02 <alan.social02@gmail.com>
2023-05-25 21:10:39 +0800

    adds basic keypad to calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const KEY = ['AC', '/', 'X', 7, 8, 9, '-', 4, 5, 6, '+', 1, 2, 3, '=', 0, '.'];
+
+function KeyPad({ handleKeyInput }) {
+	const keyArray = KEY.map((item, index) => {
+		return(
+			<div
+				className="table text-center p-5 rounded-md"
+				key={index + 1} 
+				id={'key' + (index + 1)}
+				onClick={() => handleKeyInput(item)}>
+				<p className="table-cell align-middle select-none">{item}</p>
+			</div>
+		);
+	});
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-          Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-          Learn React
-				</a>
-			</header>
+		<div id="grid-container" className="w-full h-full">
+			{keyArray}
 		</div>
 	);
 }
 
-export default App;
+function TextDisplay() {
+	return (
+		<div className="bg-[#232323] w-fill h-28"></div>
+	);
+}
+
+export default function App() {
+
+	function handleKeyInput(pressedKey) {
+		switch(pressedKey) {
+		case 'AC':
+			console.log('AC');
+			break;
+		case '/':
+			console.log('/');
+			break;
+		case 'X':
+			console.log('X');
+			break;
+		case '+':
+			console.log('+');
+			break;
+		case '=':
+			console.log('=');
+			break;
+		case 7:
+			console.log('is a number');
+			break;
+		}
+	}
+
+	return (
+		<div className="App">
+			<div className="flex flex-col justify-center mt-20 my-auto mx-auto w-[500px] h-[650px]">
+				<TextDisplay/>
+				<KeyPad handleKeyInput={handleKeyInput}/>
+			</div>
+		</div>
+	);
+}
